19d2b65fdeafd80a61144a421a72b67a3c35025b
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2012-11-13 10:39:50 +0800

    deploy task work on all web servers not only app server

--- lib/capistrano-unicorn/capistrano_integration.rb
+++ lib/capistrano-unicorn/capistrano_integration.rb
@@ -119,22 +119,22 @@ module CapistranoUnicorn
         #
         namespace :unicorn do
           desc 'Start Unicorn master process'
-          task :start, :roles => :app, :except => {:no_release => true} do
+          task :start, :roles => :web, :except => {:no_release => true} do
             run start_unicorn
           end
 
           desc 'Stop Unicorn'
-          task :stop, :roles => :app, :except => {:no_release => true} do
+          task :stop, :roles => :web, :except => {:no_release => true} do
             run kill_unicorn('QUIT')
           end
 
           desc 'Immediately shutdown Unicorn'
-          task :shutdown, :roles => :app, :except => {:no_release => true} do
+          task :shutdown, :roles => :web, :except => {:no_release => true} do
             run kill_unicorn('TERM')
           end
 
           desc 'Restart Unicorn'
-          task :restart, :roles => :app, :except => {:no_release => true} do
+          task :restart, :roles => :web, :except => {:no_release => true} do
             run <<-END
               if #{unicorn_is_running?}; then
                 echo "Restarting Unicorn...";
@@ -146,7 +146,7 @@ module CapistranoUnicorn
           end
 
           desc 'Reload Unicorn'
-          task :reload, :roles => :app, :except => {:no_release => true} do
+          task :reload, :roles => :web, :except => {:no_release => true} do
             run <<-END
               if #{unicorn_is_running?}; then
                 echo "Reloading Unicorn...";
@@ -158,7 +158,7 @@ module CapistranoUnicorn
           end
 
           desc 'Add a new worker'
-          task :add_worker, :roles => :app, :except => {:no_release => true} do
+          task :add_worker, :roles => :web, :except => {:no_release => true} do
             run <<-END
               if #{unicorn_is_running?}; then
                 echo "Adding a new Unicorn worker...";
@@ -170,7 +170,7 @@ module CapistranoUnicorn
           end
 
           desc 'Remove amount of workers'
-          task :remove_worker, :roles => :app, :except => {:no_release => true} do
+          task :remove_worker, :roles => :web, :except => {:no_release => true} do
             run <<-END
               if #{unicorn_is_running?}; then
                 echo "Removing a Unicorn worker...";
